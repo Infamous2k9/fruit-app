b28f0495a39f91f621a91fa1025bf54687a4c49f
Andre Funk <Andre.funk.94@web.de>
2026-07-17 15:33:31 +0200

    create 5 fruits and get a response on "fruits/"

--- fruit_app/views.py
+++ fruit_app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def send_fruits(request):
+    fruits = [
+        {
+            "name": "Apfel",
+            "gewicht": 180,
+            "farbe": "Rot",
+        },
+        {
+            "name": "Banane",
+            "gewicht": 120,
+            "farbe": "Gelb",
+        },
+        {
+            "name": "Orange",
+            "gewicht": 200,
+            "farbe": "Orange",
+        },
+        {
+            "name": "Kiwi",
+            "gewicht": 90,
+            "farbe": "Braun",
+        },
+        {
+            "name": "Traube",
+            "gewicht": 8,
+            "farbe": "Lila",
+        },
+    ]
+
+    return JsonResponse(
+        fruits, safe=False
+    )  # erwartet standardmäßig ein Dictionary. Da wir eine Liste zurückgeben muss safe=False gesetzt werden.
